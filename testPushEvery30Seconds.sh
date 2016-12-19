dc6b2cddb7c53a5bc990a09de2f4d69f7ddb0d4a
kpakur <k.pakur@kainos.com>
2016-12-19 11:09:48 +0100

    sample file changed automatically

--- testPushEvery30Seconds.sh
+++ testPushEvery30Seconds.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-dt=$(date '+%d/%m/%Y %H:%M:%S');
-echo "$dt"
+# modifies sample file every 30 seconds and commits and pushes
+
+COUNTER=100
+until [  $COUNTER -lt 10 ]; do
+	echo COUNTER $COUNTER
+	
+	dt=$(date '+%d/%m/%Y %H:%M:%S');
+	echo "$dt"
+	echo "$dt" > "SampleFileToModify.txt"
+	
+	git commit -a -m "sample file changed automatically"
+	git push
+	
+	sleep 30
+	let COUNTER-=1
+done
